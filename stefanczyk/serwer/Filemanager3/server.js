3e06f50f571a594e79a049ef94e12d2db6213349
RobsonBeard <ropciuszek@gmail.com>
2023-02-25 15:18:34 +0100

    feat: Filemanager cz.3 caly

--- stefanczyk/serwer/Filemanager3/server.js
+++ stefanczyk/serwer/Filemanager3/server.js
@@ -15,132 +15,26 @@ app.use(
   })
 );
 
-let przechowujePliki = [];
-let j = 0; // zliczanie ID
 let nowyKontekst = {};
-let testTab;
-let sciezkaIkonki;
 
 app.post("/handleUpload", function (req, res) {
-  //możliwość zaznaczenia i uploadu jednego lub kilku plików
   let form = formidable({});
 
   form.multiples = true;
   form.keepExtensions = true;
-  form.uploadDir = __dirname + "/pliki/";
+
+  form.on('field', function (field, value) {
+    form.uploadDir = path.join(__dirname, 'pliki', value)
+    // console.log(form.uploadDir)
+  }) // kolejność inputów w hbsie ma znaczenie
+
+  form.on('fileBegin', function (name, file) {
+    file.path = form.uploadDir + '/' + file.name
+    // console.log(file.path);
+  })
 
   form.parse(req, function (err, fields, files) {
-    console.log("----- przesłane formularzem pliki ------");
-
-    console.log(files);
-
-    // if (JSON.stringify(files.plik) === '{}') {
-
-    // } //puste, ale dałem na inpucie, że wymagane jest przesłanie pliku, więc raczej nie może wystąpić
-    // else
-    if (Array.isArray(files.plik)) {
-      for (let i = 0; i < files.plik.length; i++) {
-        testTab = files.plik[i].type.split("/");
-        // console.log(testTab);
-        switch (testTab[1]) {
-          case "javascript":
-            sciezkaIkonki =
-              "https://cdn-icons-png.flaticon.com/512/337/337941.png";
-            break;
-          case "jpeg":
-            sciezkaIkonki =
-              "https://cdn-icons-png.flaticon.com/512/337/337940.png";
-            break;
-          case "css":
-            sciezkaIkonki =
-              "https://cdn-icons-png.flaticon.com/512/337/337928.png";
-            break;
-          case "msword":
-            sciezkaIkonki =
-              "https://cdn-icons-png.flaticon.com/512/337/337932.png";
-            break;
-          case "pdf":
-            sciezkaIkonki =
-              "https://cdn-icons-png.flaticon.com/512/337/337946.png";
-            break;
-          case "png":
-            sciezkaIkonki =
-              "https://cdn-icons-png.flaticon.com/512/337/337948.png";
-            break;
-          case "x-zip-compressed":
-            sciezkaIkonki =
-              "https://cdn-icons-png.flaticon.com/512/337/337960.png";
-            break;
-          default:
-            sciezkaIkonki =
-              "https://cdn-icons-png.flaticon.com/512/4725/4725478.png";
-        }
-        let pom = {
-          id: j,
-          name: files.plik[i].name,
-          path: files.plik[i].path,
-          size: files.plik[i].size,
-          type: files.plik[i].type,
-          savedate: new Date().getTime(),
-          image: sciezkaIkonki
-        };
-        przechowujePliki.push(pom);
-        j++;
-      }
-    } //tablica
-    else {
-      testTab = files.plik.type.split("/");
-      switch (testTab[1]) {
-        case "javascript":
-          sciezkaIkonki =
-            "https://cdn-icons-png.flaticon.com/512/337/337941.png";
-          break;
-        case "jpeg":
-          sciezkaIkonki =
-            "https://cdn-icons-png.flaticon.com/512/337/337940.png";
-          break;
-        case "css":
-          sciezkaIkonki =
-            "https://cdn-icons-png.flaticon.com/512/337/337928.png";
-          break;
-        case "msword":
-          sciezkaIkonki =
-            "https://cdn-icons-png.flaticon.com/512/337/337932.png";
-          break;
-        case "pdf":
-          sciezkaIkonki =
-            "https://cdn-icons-png.flaticon.com/512/337/337946.png";
-          break;
-        case "png":
-          sciezkaIkonki =
-            "https://cdn-icons-png.flaticon.com/512/337/337948.png";
-          break;
-        case "x-zip-compressed":
-          sciezkaIkonki =
-            "https://cdn-icons-png.flaticon.com/512/337/337960.png";
-          break;
-        default:
-          sciezkaIkonki =
-            "https://cdn-icons-png.flaticon.com/512/4725/4725478.png";
-      }
-      let pom = {
-        id: j,
-        name: files.plik.name,
-        path: files.plik.path,
-        size: files.plik.size,
-        type: files.plik.type,
-        savedate: new Date().getTime(),
-        image: sciezkaIkonki
-      };
-      przechowujePliki.push(pom);
-      j++;
-    } //obiekt
-
-    nowyKontekst.przechowane = przechowujePliki;
-    console.log("----- kontekst: ");
-    console.log(nowyKontekst);
-
-    res.redirect("/filemanager");
+    res.redirect(`/filemanager?name=${fields.sciezka}`);
   });
 });
 
@@ -157,98 +51,99 @@ app.get("/filemanager", function (req, res) {
   const filepath = (req.query.name) ? path.join(__dirname, "pliki", req.query.name) : path.join(__dirname, "pliki");
   // skrócony if
 
-  fs.readdir(filepath, (err, files) => {
-    // mozna readdirSync zobaczyc
+  // console.log(filepath);
 
-    mamKatalogi = [];
-    mamPliki = [];
+  if (fs.existsSync(filepath)) {
+    fs.readdir(filepath, (err, files) => {
+      // mozna readdirSync zobaczyc
 
-    if (err) throw err;
+      mamKatalogi = [];
+      mamPliki = [];
 
-    for (let i = 0; i < files.length; i++) {
-      fs.lstat(path.join(filepath, files[i]), (err, stats) => {
-        if (stats.isDirectory()) {
-          let pom2 = {
-            name: files[i]
-          };
-          mamKatalogi.push(pom2);
-        } else {
-          let rozszerzenie = files[i].split(".");
-          let obrazPliku;
-          switch (rozszerzenie[rozszerzenie.length - 1]) {
-            case "js":
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/337/337941.png";
-              break;
-            case "jpeg":
-            case "jpg":
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/337/337940.png";
-              break;
-            case "css":
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/337/337928.png";
-              break;
-            case "txt":
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/337/337956.png";
-              break;
-            case "docx":
-            case "doc":
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/337/337932.png";
-              break;
-            case "pdf":
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/337/337946.png";
-              break;
-            case "png":
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/337/337948.png";
-              break;
-            case "php":
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/337/337947.png";
-              break;
-            case "html":
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/337/337937.png";
-              break;
-            case "zip":
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/337/337960.png";
-              break;
-            default:
-              obrazPliku =
-                "https://cdn-icons-png.flaticon.com/512/4725/4725478.png";
+      if (err) throw err;
+
+      for (let i = 0; i < files.length; i++) {
+        fs.lstat(path.join(filepath, files[i]), (err, stats) => {
+          if (stats.isDirectory()) {
+            let pom2 = {
+              name: files[i]
+            };
+            mamKatalogi.push(pom2);
+          } else {
+            let rozszerzenie = files[i].split(".");
+            let obrazPliku;
+            switch (rozszerzenie[rozszerzenie.length - 1]) {
+              case "js":
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/337/337941.png";
+                break;
+              case "jpeg":
+              case "jpg":
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/337/337940.png";
+                break;
+              case "css":
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/337/337928.png";
+                break;
+              case "txt":
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/337/337956.png";
+                break;
+              case "docx":
+              case "doc":
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/337/337932.png";
+                break;
+              case "pdf":
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/337/337946.png";
+                break;
+              case "png":
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/337/337948.png";
+                break;
+              case "php":
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/337/337947.png";
+                break;
+              case "html":
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/337/337937.png";
+                break;
+              case "zip":
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/337/337960.png";
+                break;
+              default:
+                obrazPliku =
+                  "https://cdn-icons-png.flaticon.com/512/4725/4725478.png";
+            }
+            let pom3 = {
+              name: files[i],
+              image: obrazPliku
+            };
+            mamPliki.push(pom3);
           }
-          let pom3 = {
-            name: files[i],
-            image: obrazPliku
-          };
-          mamPliki.push(pom3);
-        }
-      });
-    }
+        });
+      }
 
-    console.log(req.query);
+      // console.log(req.query);
 
-    nowyKontekst.sciezka = req.query.name
-    nowyKontekst.katalogi = mamKatalogi;
-    nowyKontekst.pliki = mamPliki;
+      nowyKontekst.sciezka = req.query.name
+      nowyKontekst.katalogi = mamKatalogi;
+      nowyKontekst.pliki = mamPliki;
 
-    // console.log(nowyKontekst);
+      // console.log(nowyKontekst.sciezka);
 
-    res.render("filemanager.hbs", nowyKontekst);
-  });
+      res.render("filemanager.hbs", nowyKontekst);
+    });
+  }
+  else {
+    res.redirect("/filemanager")
+  }
   // console.log(nowyKontekst);
 });
-
-// app.post("/fetch", function (req, res) {
-
-//   res.setHeader("content-type", "application/json");
-//   res.send(JSON.stringify(req.body, null, 3));
-// });
 
 app.get("/nowyKatalog", function (req, res) {
   let daneKatalog = req.query;
@@ -273,41 +168,69 @@ app.get("/nowyKatalog", function (req, res) {
 
 app.get("/nowyPlik", function (req, res) {
   let danePliku = req.query;
-  const filepath = path.join(__dirname, "pliki", `${danePliku.nazwa}.txt`);
+
 
   if (danePliku.nazwa !== "") {
+    const filepath = path.join(__dirname, "pliki", req.query.sciezka, req.query.nazwa);
     if (!fs.existsSync(filepath)) {
       fs.writeFile(filepath, "", err => {
         if (err) throw err;
         console.log(`stworzono plik o nazwie ${danePliku.nazwa}.txt`);
       });
 
-      res.redirect("/filemanager");
+      res.redirect(`/filemanager?name=${req.query.sciezka}`);
     } else {
       console.log(`plik o nazwie ${danePliku.nazwa}.txt już istnieje`);
-      res.redirect("/filemanager");
+      res.redirect(`/filemanager?name=${req.query.sciezka}`);
     }
   } else {
     console.log(`podano pustą nazwę`);
-    res.redirect("/filemanager");
+    res.redirect(`/filemanager?name=${req.query.sciezka}`);
   }
 });
+
+app.get("/zmianaNazwy", function (req, res) {
+  const new_dir = req.query.sciezka.substring(0, req.query.sciezka.lastIndexOf('/') + 1) + req.query.nazwa
+  // substring robi: halo/malo -> halo/
+  const new_path = path.join(__dirname, "pliki", new_dir)
+  const old_path = path.join(__dirname, 'pliki', req.query.sciezka)
+
+  // console.log(new_path);
+  // console.log(old_path)
+
+  if (!fs.existsSync(new_path)) {
+    fs.rename(old_path, new_path, (err) => {
+      if (err) console.log(err)
+      else {
+        res.redirect(`/filemanager?name=${new_dir}`)
+      }
+    })
+  }
+  else {
+    res.redirect(`/filemanager?name=${req.query.sciezka}`)
+  }
+
+})
 
 app.post("/usunKatalog", function (req, res) {
   const filepath = path.join(__dirname, "pliki", req.body.nazwa)
   if (fs.existsSync(filepath)) {
-    fs.rmdir(path.join(__dirname, "pliki", `${req.body.nazwa}`), err => {
-      console.log(`usunięto katalog o nazwie ${req.body.nazwa}`);
-      res.redirect("/filemanager");
-    });
+    fs.rmSync(filepath, {
+      recursive: true,
+      force: true
+    })
+    console.log(`usunięto katalog o nazwie ${req.body.nazwa}`);
+    res.redirect(`/filemanager?name=${req.body.sciezka}`);
+
   }
 });
 
 app.post("/usunPlik", function (req, res) {
-  if (fs.existsSync(path.join(__dirname, "pliki", `${req.body.nazwa}`))) {
-    fs.unlink(path.join(__dirname, "pliki", `${req.body.nazwa}`), err => {
+  const filepath = path.join(__dirname, "pliki", req.body.nazwa)
+  if (fs.existsSync(filepath)) {
+    fs.unlink(filepath, err => {
       console.log(`usunięto plik o nazwie ${req.body.nazwa}`);
-      res.redirect("/filemanager");
+      res.redirect(`/filemanager?name=${req.body.sciezka}`);
     });
   }
 });
@@ -391,6 +314,28 @@ app.engine(
           return wynik;
         } else {
           return title;
+        }
+      },
+      filePath: function (sciezka) {
+        if (sciezka == undefined) {
+          return `<a href="/">home</a>`
+        }
+        else {
+          let arraySciezka = sciezka.split('/')
+          arraySciezka[0] = 'home'
+          let zliczacz
+          for (let i = 0; i < arraySciezka.length; i++) {
+            if (i == 0) {
+              arraySciezka[i] = `<a href="/filemanager">${arraySciezka[i]}</a>`
+              zliczacz = "/filemanager?name="
+            }
+            else {
+              zliczacz += `/${arraySciezka[i]}`
+              arraySciezka[i] = `<a href="${zliczacz}">${arraySciezka[i]}</a>`
+            }
+          }
+          let stringSciezka = arraySciezka.join(`<p style="display: inline;"> > </p>`)
+          return stringSciezka
         }
       }
     }
--- stefanczyk/serwer/Filemanager3/static/js/dialog.js
+++ stefanczyk/serwer/Filemanager3/static/js/dialog.js
@@ -1,8 +1,41 @@
 let nowyFolder = document.getElementById("nowyFolder")
-nowyFolder.addEventListener("click", otworzDialog)
-let dialog = document.getElementById("dialog")
+nowyFolder.addEventListener("click", otworzDialog1)
+let dialog1 = document.getElementById("dialog1")
 
-function otworzDialog() {
-    console.log(dialog);
-    dialog.open = true
+let nowyPlik = document.getElementById("nowyPlik")
+nowyPlik.addEventListener("click", otworzDialog2)
+let dialog2 = document.getElementById("dialog2")
+
+let dialog3 = document.getElementById("dialog3")
+let zmianaNazwy = document.getElementById("zmianaNazwy")
+zmianaNazwy.addEventListener("click", otworzDialog3)
+
+function otworzDialog1() {
+    dialog1.open = true
+    dialog2.open = false
+    dialog3.open = false
+}
+
+function otworzDialog2() {
+    dialog1.open = false
+    dialog2.open = true
+    dialog3.open = false
+}
+
+function otworzDialog3() {
+    dialog1.open = false
+    dialog2.open = false
+    dialog3.open = true
+}
+
+function zamknijDialog1() {
+    dialog1.open = false
+}
+
+function zamknijDialog2() {
+    dialog2.open = false
+}
+
+function zamknijDialog3() {
+    dialog3.open = false
 }
