e5d73597976ff1460e7b8cbe7fc050e4ade72419
RobsonBeard <ropciuszek@gmail.com>
2023-03-27 20:41:14 +0200

    reactJS cz.4 poczatek

---
+++ stefanczyk/klient/react4/testapp001/src/App02/App02.js
@@ -0,0 +1,14 @@
+import './App02.css'
+import Item01 from './Item02'
+
+
+const App2 = () => {
+  return (
+    <div >
+      <p>AAAAAAAAAAAAAAAAA</p>
+      <Item01 />
+    </div>
+  );
+}
+
+export default App2;
