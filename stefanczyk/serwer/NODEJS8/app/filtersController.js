0391dcd0a9f8aad6a483df5f797ddec95217a066
RobsonBeard <ropciuszek@gmail.com>
2023-05-14 17:38:06 +0200

    nodejs8 postępy

--- stefanczyk/serwer/NODEJS8/app/filtersController.js
+++ stefanczyk/serwer/NODEJS8/app/filtersController.js
@@ -3,7 +3,7 @@
 //* modyfikacje zdjęć
 
 const sharp = require('sharp') // metody stosowania na https://www.npmjs.com/package/sharp
-// const logger = require('tracer').colorConsole()
+const logger = require('tracer').colorConsole()
 
 const jsonController = require('./jsonController')
 
@@ -35,18 +35,18 @@ const getMetadata = async (selectedID) => {
 
 const makeFilteredImage = async (modificationData) => {
   const gotImageJSON = await jsonController.getone(modificationData.id)
-  return new Promise((resolve, reject) => { // TODO: zrobic tak, zeby mozna bylo np. ustawic dowolny kolor, sprawdzac czy zmienne zostaly ustawione
+  return new Promise((resolve, reject) => { // TODO: sprawdzac czy zmienne zostaly ustawione
     try {
       if (gotImageJSON.success) {
         const selectedImage = gotImageJSON.result
 
-        const originalURL = selectedImage.url // TODO: chyba lepiej da się odciąć rozszerzenie
+        const originalURL = selectedImage.url
 
-        let newURL = originalURL.split('.')
-        const extension = newURL.pop()
-        newURL = newURL.join('.')
-        newURL = `${newURL}-${modificationData.status}.${extension}`
-
+        const splitURL = originalURL.split('.')
+        const extension = splitURL.pop()
+        const joinedURL = splitURL.join('.')
+        const newURL = `${joinedURL}-${modificationData.status}.${extension}`
+        let newFormatURL
         // TODO: to nie jest zrobione tak jak byc powinno. jak zrobie kilka razy tinta, to chyba powinno sie wygenerowac kilka kopii zdjecia i w updacie historii w jsonie powinny byc osobne urle do tego. np: robie tinta na czerwono, a potem znowu, tylko ze na zielono. oba linki beda prowadzic do zielonego zdjecia
 
         const updateImageHistory = () => {
@@ -59,12 +59,94 @@ const makeFilteredImage = async (modificationData) => {
             sharp(originalURL)
               .tint({ r: modificationData.params.red, g: modificationData.params.green, b: modificationData.params.blue })
               .toFile(newURL)
-              .then(async () => {
+              .then(() => {
                 updateImageHistory()
                 resolve({ success: true, message: `${modificationData.status} zakończono pomyślnie`, result: selectedImage })
               })
+              .catch(error => logger.log(error))
             break
-          // TODO: reszta operacji
+          case 'rotate':
+            sharp(originalURL)
+              .rotate(modificationData.params.degrees)
+              .toFile(newURL)
+              .then(() => {
+                updateImageHistory()
+                resolve({ success: true, message: `${modificationData.status} zakończono pomyślnie`, result: selectedImage })
+              })
+              .catch(error => logger.log(error))
+            break
+          case 'resize':
+            sharp(originalURL)
+              .resize({ width: modificationData.params.width, height: modificationData.params.height })
+              .toFile(newURL)
+              .then(() => {
+                updateImageHistory()
+                resolve({ success: true, message: `${modificationData.status} zakończono pomyślnie`, result: selectedImage })
+              })
+              .catch(error => logger.log(error))
+            break
+          case 'reformat':
+            newFormatURL = `${joinedURL}-${modificationData.status}.${modificationData.params.newFormat}`
+            sharp(originalURL)
+              .toFormat(modificationData.params.newFormat)
+              .toFile(newFormatURL)
+              .then(() => {
+                selectedImage.history.push({ status: modificationData.status, timestamp: new Date().getTime(), url: newFormatURL })
+                selectedImage.lastChange = modificationData.status // nie uzywam funkcji bo musze uzyc innego urla
+                resolve({ success: true, message: `${modificationData.status} zakończono pomyślnie`, result: selectedImage })
+              })
+              .catch(error => logger.log(error))
+            break
+          case 'crop':
+            sharp(originalURL)
+              .extract({ width: modificationData.params.width, height: modificationData.params.height, left: modificationData.params.left, top: modificationData.params.top })
+              .toFile(newURL)
+              .then(() => {
+                updateImageHistory()
+                resolve({ success: true, message: `${modificationData.status} zakończono pomyślnie`, result: selectedImage })
+              })
+              .catch(error => logger.log(error))
+            break
+          case 'grayscale':
+            sharp(originalURL)
+              .grayscale()
+              .toFile(newURL)
+              .then(() => {
+                updateImageHistory()
+                resolve({ success: true, message: `${modificationData.status} zakończono pomyślnie`, result: selectedImage })
+              })
+              .catch(error => logger.log(error))
+            break
+          case 'flip':
+            sharp(originalURL)
+              .flip()
+              .toFile(newURL)
+              .then(() => {
+                updateImageHistory()
+                resolve({ success: true, message: `${modificationData.status} zakończono pomyślnie`, result: selectedImage })
+              })
+              .catch(error => logger.log(error))
+            break
+          case 'flop':
+            sharp(originalURL)
+              .flop()
+              .toFile(newURL)
+              .then(() => {
+                updateImageHistory()
+                resolve({ success: true, message: `${modificationData.status} zakończono pomyślnie`, result: selectedImage })
+              })
+              .catch(error => logger.log(error))
+            break
+          case 'negate':
+            sharp(originalURL)
+              .negate()
+              .toFile(newURL)
+              .then(() => {
+                updateImageHistory()
+                resolve({ success: true, message: `${modificationData.status} zakończono pomyślnie`, result: selectedImage })
+              })
+              .catch(error => logger.log(error))
+            break
           default:
             resolve({ success: false, message: 'nie wybrano metody operacji na zdjęciu lub jest ona nieznana' })
         }
